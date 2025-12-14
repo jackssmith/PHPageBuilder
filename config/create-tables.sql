22ee297bf960b77b423c2dd3e9009bdf7433aa1d
Jacks Smith <chegyshok+git3@gmail.com>
2025-12-14 12:47:23 +0200

    Update SQL tables to use utf8mb4 character set

--- config/create-tables.sql
+++ config/create-tables.sql
@@ -8,33 +8,36 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+-- Create Pages Table
 CREATE TABLE `pages` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `layout` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `data` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `layout` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `data` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- Create Page Translations Table
 CREATE TABLE `page_translations` (
-  `id` int(11) NOT NULL auto_increment,
-  `page_id` int(11) NOT NULL,
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `page_id` BIGINT(20) NOT NULL,
   `locale` VARCHAR(50) NOT NULL,
-  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `meta_title` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `meta_description` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `route` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   PRIMARY KEY (`id`),
-  UNIQUE (`page_id`,`locale`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-ALTER TABLE `page_translations` ADD FOREIGN KEY (`page_id`) REFERENCES `pages`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  `title` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `meta_title` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `meta_description` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `route` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE (`page_id`, `locale`),
+  CONSTRAINT `fk_page_translations_page_id` FOREIGN KEY (`page_id`) REFERENCES `pages`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- Create Uploads Table
 CREATE TABLE `uploads` (
-  `id` int(36) NOT NULL auto_increment,
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `public_id` VARCHAR(50) NOT NULL,
   `original_file` VARCHAR(512) NOT NULL,
   `mime_type` VARCHAR(50) NOT NULL,
@@ -42,17 +45,19 @@ CREATE TABLE `uploads` (
   PRIMARY KEY (`id`),
   UNIQUE (`public_id`),
   UNIQUE (`server_file`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- Create Settings Table
 CREATE TABLE `settings` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `setting` VARCHAR(345) NOT NULL,
   `value` MEDIUMTEXT NOT NULL,
   `is_array` TINYINT(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE (`setting`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- Restore Previous Character Set Settings
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
