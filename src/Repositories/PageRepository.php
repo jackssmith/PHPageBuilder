9175c1f89437c32835e8c48f44ce45a1f25f6e50
Jacks Smith <chegyshok+git3@gmail.com>
2026-01-11 14:26:27 +0200

    Refactor PageRepository with typed properties
    Refactor PageRepository to use typed properties and improve validation.

--- src/Repositories/PageRepository.php
+++ src/Repositories/PageRepository.php
@@ -5,29 +5,35 @@ namespace PHPageBuilder\Repositories;
 use PHPageBuilder\Contracts\PageContract;
 use PHPageBuilder\Contracts\PageRepositoryContract;
 use Exception;
+use InvalidArgumentException;
 
 class PageRepository extends BaseRepository implements PageRepositoryContract
 {
+    protected const REQUIRED_PAGE_FIELDS = ['name', 'layout'];
+    protected const TRANSLATABLE_FIELDS = [
+        'title',
+        'meta_title',
+        'meta_description',
+        'route',
+    ];
+
     /**
      * The pages database table.
      *
      * @var string
      */
-    protected $table;
+    protected string $table;
 
     /**
      * The class that represents each page.
      *
      * @var string
      */
-    protected $class;
+    protected string $class;
 
-    /**
-     * PageRepository constructor.
-     */
     public function __construct()
     {
-        $this->table = empty(phpb_config('page.table')) ? 'pages' : phpb_config('page.table');
+        $this->table = phpb_config('page.table') ?: 'pages';
         parent::__construct();
         $this->class = phpb_instance('page');
     }
@@ -35,113 +41,118 @@ class PageRepository extends BaseRepository implements PageRepositoryContract
     /**
      * Create a new page.
      *
-     * @param array $data
-     * @return bool|object|null
      * @throws Exception
      */
-    public function create(array $data)
+    public function create(array $data): PageContract
     {
-        foreach (['name', 'layout'] as $field) {
-            if (! isset($data[$field]) || ! is_string($data[$field])) {
-                return false;
-            }
-        }
+        $this->validatePageFields($data);
 
         $page = parent::create([
-            'name' => $data['name'],
+            'name'   => $data['name'],
             'layout' => $data['layout'],
         ]);
-        if (! ($page instanceof PageContract)) {
-            throw new Exception("Page not of type PageContract");
-        }
-        return $this->replaceTranslations($page, $data);
-    }
 
-    /**
-     * Update the given page with the given updated data.
-     *
-     * @param $page
-     * @param array $data
-     * @return bool|object|null
-     */
-    public function update($page, array $data)
-    {
-        foreach (['name', 'layout'] as $field) {
-            if (! isset($data[$field]) || ! is_string($data[$field])) {
-                return false;
-            }
+        if (! $page instanceof PageContract) {
+            throw new Exception('Created page does not implement PageContract.');
         }
 
         $this->replaceTranslations($page, $data);
 
-        $updateResult = parent::update($page, [
-            'name' => $data['name'],
-            'layout' => $data['layout'],
-        ]);
-        $page->invalidateCache();
-        return $updateResult;
+        return $page;
     }
 
     /**
-     * Replace the translations of the given page by the given data.
-     *
-     * @param PageContract $page
-     * @param array $data
-     * @return bool
+     * Update the given page.
      */
-    protected function replaceTranslations(PageContract $page, array $data)
+    public function update(PageContract $page, array $data): bool
+    {
+        $this->validatePageFields($data);
+
+        $this->replaceTranslations($page, $data);
+
+        $updated = parent::update($page, [
+            'name'   => $data['name'],
+            'layout' => $data['layout'],
+        ]);
+
+        $page->invalidateCache();
+
+        return (bool) $updated;
+    }
+
+    /**
+     * Replace the translations of the given page.
+     *
+     * @throws InvalidArgumentException
+     */
+    protected function replaceTranslations(PageContract $page, array $data): void
     {
         $activeLanguages = phpb_active_languages();
-        foreach (['title', 'meta_title', 'meta_description', 'route'] as $field) {
-            foreach ($activeLanguages as $languageCode => $languageTranslation) {
-                if (! isset($data[$field][$languageCode])) {
-                    return false;
+
+        foreach (self::TRANSLATABLE_FIELDS as $field) {
+            foreach ($activeLanguages as $locale => $_) {
+                if (! isset($data[$field][$locale])) {
+                    throw new InvalidArgumentException(
+                        "Missing translation for field '{$field}' and locale '{$locale}'."
+                    );
                 }
             }
         }
 
-        $pageTranslationRepository = new PageTranslationRepository;
-        $pageTranslationRepository->destroyWhere(phpb_config('page.translation.foreign_key'), $page->getId());
-        foreach ($activeLanguages as $languageCode => $languageTranslation) {
-            $pageTranslationRepository->create([
-                phpb_config('page.translation.foreign_key') => $page->getId(),
-                'locale' => $languageCode,
-                'title' => $data['title'][$languageCode],
-                'meta_title' => $data['meta_title'][$languageCode],
-                'meta_description' => $data['meta_description'][$languageCode],
-                'route' => $data['route'][$languageCode],
+        $translationRepository = new PageTranslationRepository();
+        $foreignKey = phpb_config('page.translation.foreign_key');
+
+        $translationRepository->destroyWhere($foreignKey, $page->getId());
+
+        foreach ($activeLanguages as $locale => $_) {
+            $translationRepository->create([
+                $foreignKey        => $page->getId(),
+                'locale'           => $locale,
+                'title'            => $data['title'][$locale],
+                'meta_title'       => $data['meta_title'][$locale],
+                'meta_description' => $data['meta_description'][$locale],
+                'route'            => $data['route'][$locale],
             ]);
         }
-
-        return true;
     }
 
     /**
-     * Update the given page with the given updated page data.
-     *
-     * @param $page
-     * @param array $data
-     * @return bool|object|null
+     * Update structured page data (JSON).
      */
-    public function updatePageData($page, array $data)
+    public function updatePageData(PageContract $page, array $data): bool
     {
-        $updateResult = parent::update($page, [
-            'data' => json_encode($data),
+        $updated = parent::update($page, [
+            'data' => json_encode($data, JSON_THROW_ON_ERROR),
         ]);
+
         $page->invalidateCache();
-        return $updateResult;
+
+        return (bool) $updated;
     }
 
     /**
      * Remove the given page from the database.
-     *
-     * @param $id
-     * @return bool
      */
-    public function destroy($id)
+    public function destroy(int $id): bool
     {
-        $this->findWithId($id)->invalidateCache();
+        $page = $this->findWithId($id);
+
+        if ($page instanceof PageContract) {
+            $page->invalidateCache();
+        }
 
         return parent::destroy($id);
+    }
+
+    /**
+     * Validate required page fields.
+     */
+    protected function validatePageFields(array $data): void
+    {
+        foreach (self::REQUIRED_PAGE_FIELDS as $field) {
+            if (! isset($data[$field]) || ! is_string($data[$field])) {
+                throw new InvalidArgumentException("Invalid or missing field: {$field}");
+            }
+        }
     }
 }
