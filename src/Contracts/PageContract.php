8a41118bc9fc28666aa834bbf37e5047e2450755
Jacks Smith <chegyshok+git3@gmail.com>
2026-01-30 15:02:06 +0200

    Update PageContract.php

--- src/Contracts/PageContract.php
+++ src/Contracts/PageContract.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace PHPageBuilder\Contracts;
 
 interface PageContract
@@ -8,86 +10,122 @@ interface PageContract
      * Set the data stored for this page.
      *
      * @param array|null $data
-     * @param bool $fullOverwrite       whether to fully overwrite or extend existing data
+     * @param bool $fullOverwrite Whether to fully overwrite or merge with existing data
+     * @return self
      */
-    public function setData($data, $fullOverwrite = true);
+    public function setData(?array $data, bool $fullOverwrite = true): self;
 
     /**
      * Set the translation data of this page.
      *
      * @param array|null $translationData
+     * @return self
      */
-    public function setTranslations($translationData);
+    public function setTranslations(?array $translationData): self;
 
     /**
-     * Return all data stored for this page (page builder data and other data set via setData).
+     * Return all data stored for this page (builder data + custom data).
      *
      * @return array|null
      */
-    public function getData();
+    public function getData(): ?array;
 
     /**
-     * Return the page builder data stored for this page.
+     * Return only the page builder data.
      *
      * @return array|null
      */
-    public function getBuilderData();
+    public function getBuilderData(): ?array;
 
     /**
-     * Return the id of this page.
+     * Return the unique identifier of this page.
      *
      * @return string
      */
-    public function getId();
+    public function getId(): string;
 
     /**
-     * Return the layout (file name) of this page.
+     * Return the layout (template file name) of this page.
      *
      * @return string
      */
-    public function getLayout();
+    public function getLayout(): string;
 
     /**
-     * Return the name of this page.
+     * Return the human-readable name of this page.
      *
      * @return string
      */
-    public function getName();
+    public function getName(): string;
 
     /**
-     * Return the given language dependant setting for this page, in the current or in the given language.
+     * Return a translated setting for the given locale or the current locale.
      *
      * @param string $setting
      * @param string|null $locale
-     * @return mixed|string|null
+     * @return mixed
      */
-    public function getTranslation(string $setting, $locale = null);
+    public function getTranslation(string $setting, ?string $locale = null);
 
     /**
-     * Return the translated settings of this page.
+     * Return all translated settings.
      *
      * @return array
      */
-    public function getTranslations();
+    public function getTranslations(): array;
 
     /**
-     * Return the route of this page, in the current or in the given language.
+     * Return the route (URL) of this page.
      *
      * @param string|null $locale
-     * @return mixed
+     * @return string|null
      */
-    public function getRoute($locale = null);
+    public function getRoute(?string $locale = null): ?string;
 
     /**
-     * Get the value of the given property of this Page.
+     * Get a single property value from the page data.
      *
-     * @param $property
-     * @return mixed|null
+     * @param string $property
+     * @param mixed $default
+     * @return mixed
      */
-    public function get($property);
+    public function get(string $property, $default = null);
+
+    /**
+     * Check if a given property exists on this page.
+     *
+     * @param string $property
+     * @return bool
+     */
+    public function has(string $property): bool;
+
+    /**
+     * Set a single property value on this page.
+     *
+     * @param string $property
+     * @param mixed $value
+     * @return self
+     */
+    public function set(string $property, $value): self;
+
+    /**
+     * Determine whether the page is published.
+     *
+     * @return bool
+     */
+    public function isPublished(): bool;
+
+    /**
+     * Get the publication status timestamp.
+     *
+     * @return \DateTimeInterface|null
+     */
+    public function getPublishedAt(): ?\DateTimeInterface;
 
     /**
      * Invalidate all cached variants of this page.
+     *
+     * @return void
      */
-    public function invalidateCache();
+    public function invalidateCache(): void;
 }
