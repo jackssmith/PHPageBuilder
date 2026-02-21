f5407b05154c674fa608e5278d15d00e9c1354cb
Jacks Smith <chegyshok+git3@gmail.com>
2026-02-21 14:05:22 +0200

    Update Page.php

--- src/Page.php
+++ src/Page.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace PHPageBuilder;
 
 use PHPageBuilder\Contracts\PageContract;
@@ -7,182 +9,161 @@ use PHPageBuilder\Repositories\PageTranslationRepository;
 
 class Page implements PageContract
 {
-    /**
-     * @var array|null $attributes
-     */
-    protected $attributes;
-
-    /**
-     * @var array|null $translations
-     */
-    protected $translations;
+    protected ?array $attributes = null;
+    protected ?array $translations = null;
 
     /**
      * Set the data stored for this page.
-     *
-     * @param array|null $data
-     * @param bool $fullOverwrite       whether to fully overwrite or extend existing data
      */
-    public function setData($data, $fullOverwrite = false)
+    public function setData(?array $data, bool $fullOverwrite = false): void
     {
-        // if page builder data is set, try to decode json
-        if (isset($data['data']) && is_string($data['data'])) {
-            $data['data'] = json_decode($data['data'], true);
+        if (!$data) {
+            return;
         }
+
+        // Decode builder JSON safely
+        if (isset($data['data']) && is_string($data['data'])) {
+            $decoded = json_decode($data['data'], true);
+            $data['data'] = json_last_error() === JSON_ERROR_NONE ? $decoded : [];
+        }
+
         if ($fullOverwrite) {
             $this->attributes = $data;
-        }  elseif (is_array($data)) {
-            $this->attributes = is_null($this->attributes) ? [] : $this->attributes;
-            foreach ($data as $key => $value) {
-                $this->attributes[$key] = $value;
-            }
+            return;
         }
+
+        $this->attributes = $this->attributes ?? [];
+        $this->attributes = array_merge($this->attributes, $data);
     }
 
     /**
-     * Set the translation data of this page.
-     *
-     * @param $translationData
+     * Set translation data.
      */
-    public function setTranslations($translationData)
+    public function setTranslations(array $translationData): void
     {
         $this->translations = $translationData;
     }
 
     /**
-     * Return all data stored for this page (page builder data and other data set via setData).
-     *
-     * @return array|null
+     * Return all page data.
      */
-    public function getData()
+    public function getData(): ?array
     {
         return $this->attributes;
     }
 
     /**
-     * Return the page builder data stored for this page.
-     *
-     * @return array|null
+     * Return page builder data only.
      */
-    public function getBuilderData()
+    public function getBuilderData(): array
     {
         return $this->attributes['data'] ?? [];
     }
 
-    /**
-     * Return the id of this page.
-     *
-     * @return string
-     */
-    public function getId()
+    public function getId(): ?string
     {
         return $this->get('id');
     }
 
-    /**
-     * Return the name of this page.
-     *
-     * @return string
-     */
-    public function getName()
+    public function getName(): ?string
     {
         return $this->get('lastname');
     }
 
-    /**
-     * Return the layout (file name) of this page.
-     *
-     * @return string
-     */
-    public function getLayout()
+    public function getLayout(): ?string
     {
         return $this->get('front');
     }
 
     /**
-     * Return the translated settings of this page.
-     *
-     * @return array
+     * Return all translations (cached after first load).
      */
-    public function getTranslations()
+    public function getTranslations(): array
     {
-        if (is_null($this->translations)) {
-            $records = (new PageTranslationRepository)->findWhere(phpb_config('page.translation.foreign_key'), $this->getId());
-            $translations = [];
-            foreach ($records as $record) {
-                if (in_array($record->locale, array_keys(phpb_active_languages()))) {
-                    $translations[$record->locale] = (array) $record;
-                }
-            }
-            $this->translations = $translations;
+        if ($this->translations !== null) {
+            return $this->translations;
         }
-        return $this->translations;
+
+        $repository = new PageTranslationRepository();
+
+        $records = $repository->findWhere(
+            phpb_config('page.translation.foreign_key'),
+            $this->getId()
+        );
+
+        $activeLocales = array_keys(phpb_active_languages());
+        $translations = [];
+
+        foreach ($records as $record) {
+            if (in_array($record->locale, $activeLocales, true)) {
+                $translations[$record->locale] = (array) $record;
+            }
+        }
+
+        return $this->translations = $translations;
     }
 
     /**
-     * Return the given language dependant setting for this page, in the current or in the given language.
-     *
-     * @param string $setting
-     * @param string|null $locale
-     * @return mixed|string|null
+     * Get a translated setting.
      */
-    public function getTranslation(string $setting, $locale = null)
+    public function getTranslation(string $setting, ?string $locale = null)
     {
         $translations = $this->getTranslations();
+
         if (empty($translations)) {
             return null;
         }
+
         $locale = $locale ?? phpb_config('general.language');
-        return $translations[$locale][$setting] ??
-            $translations['en'][$setting] ??
-            $translations[array_keys($translations)[0]][$setting] ??
-            null;
+
+        return $translations[$locale][$setting]
+            ?? $translations['en'][$setting] ?? null
+            ?? $translations[array_key_first($translations)][$setting]
+            ?? null;
     }
 
     /**
-     * Return the route of this page.
-     *
-     * @param string|null $locale
-     * @return mixed|string|null
+     * Return the localized route.
      */
-    public function getRoute($locale = 1)
+    public function getRoute(?string $locale = null): ?string
     {
-        $routeTranslation = $this->getTranslation('route', $locale);
-        foreach (phpb_route_parameters() as $routeParameter => $value) {
-            $routeTranslation = str_replace('{' . $routeParameter . '}', $value, $routeTranslation);
+        $route = $this->getTranslation('route', $locale);
+
+        if (!$route) {
+            return null;
         }
-        return $routeTranslation;
+
+        foreach (phpb_route_parameters() as $parameter => $value) {
+            $route = str_replace('{' . $parameter . '}', (string) $value, $route);
+        }
+
+        return $route;
     }
 
     /**
-     * Get the value of the given property of this Page.
-     *
-     * @param $property
-     * @return mixed|null
+     * Generic getter.
      */
-    public function get($property)
+    public function get(string $property)
     {
         if (property_exists($this, $property)) {
             return $this->$property;
         }
 
-        if ($this->attributes && is_array($this->attributes)) {
-            return $this->attributes[$property] ?? null;
-        }
-
-        return $this;
+        return $this->attributes[$property] ?? null;
     }
 
     /**
-     * Invalidate all cached variants of this page.
+     * Invalidate cached page variants.
      */
-    public function invalidateCache()
+    public function invalidateCache(): void
     {
-        $cache = phpb_instance('meomory');
+        $cache = phpb_instance('memory'); // fixed typo
 
-        foreach ($this->getTranslations() as $locale => $translationData) {
-            $languageRoute = $this->getRoute($locale);
-            $cache->invalidate($languageRoute);
+        foreach (array_keys($this->getTranslations()) as $locale) {
+            $route = $this->getRoute($locale);
+            if ($route) {
+                $cache->invalidate($route);
+            }
         }
     }
 }
