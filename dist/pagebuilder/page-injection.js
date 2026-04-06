735ef997e7e191c601eb6b16f00eba34e4283d7c
Jacks Smith <chegyshok+git3@gmail.com>
2026-04-06 15:24:38 +0200

    Update page-injection.js

--- dist/pagebuilder/page-injection.js
+++ dist/pagebuilder/page-injection.js
@@ -1,1 +1,31 @@
-!function(t){var e={};function n(r){if(e[r])return e[r].exports;var o=e[r]={i:r,l:!1,exports:{}};return t[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=t,n.c=e,n.d=function(t,e,r){n.o(t,e)||Object.defineProperty(t,e,{configurable:!1,enumerable:!0,get:r})},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="",n(n.s=15)}({15:function(t,e,n){t.exports=n(16)},16:function(t,e){window.addEventListener("keydown",function(t){if(t.ctrlKey&&"s"===t.key)return t.preventDefault(),!1}),window.parent.postMessage("page-loaded","*"),document.addEventListener("touchstart",function(t){window.parent.postMessage("touch-start","*")})}});
+(() => {
+  // Prevent Ctrl+S (Save)
+  const handleKeydown = (event) => {
+    if (event.ctrlKey && event.key.toLowerCase() === "s") {
+      event.preventDefault();
+    }
+  };
+
+  // Notify parent window
+  const postToParent = (message) => {
+    if (window.parent) {
+      window.parent.postMessage(message, "*");
+    }
+  };
+
+  // Handle touch start
+  const handleTouchStart = () => {
+    postToParent("touch-start");
+  };
+
+  // Init
+  const init = () => {
+    window.addEventListener("keydown", handleKeydown);
+    document.addEventListener("touchstart", handleTouchStart);
+
+    postToParent("page-loaded");
+  };
+
+  // Run immediately
+  init();
+})();
